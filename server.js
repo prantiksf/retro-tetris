c403d2d89a1e3ad387922e424f176be035e6ab44
prantiksf <prantik.banerjee@salesforce.com>
2025-09-17 21:44:27 +0530

    Add Express server for Heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("/health", (req,res)=> res.json({ ok: true }));
+
+app.listen(port, ()=> {
+  console.log(`Server running on http://localhost:${port}`);
+});
